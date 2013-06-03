30134e4a87a32130c628033a9056071b921ee132
aoeztuerk <atanur.oeztuerk@uni-duesseldorf.de>
2013-06-03 16:53:43 +0200

    migrate the useremail_table to development.db

--- src/adhocracy/migration/versions/066_add_useremail_table.py
+++ src/adhocracy/migration/versions/066_add_useremail_table.py
@@ -1,10 +1,26 @@
-from sqlalchemy import MetaData, Column, Table
-from sqlalchemy import Unicode, UnicodeText
+from datetime import datetime
+from sqlalchemy import *
 
-metadata = MetaData()
+meta = MetaData()
+
+user_table = Table('user', meta,
+    Column('id', Integer, primary_key=True),
+    Column('user_name', Unicode(255), nullable=False, unique=True, index=True),
+    Column('display_name', Unicode(255), nullable=True, index=True),
+    Column('bio', UnicodeText(), nullable=True),
+    Column('email', Unicode(255), nullable=True, unique=False),
+    Column('email_priority', Integer, default=3),
+    Column('activation_code', Unicode(255), nullable=True, unique=False),
+    Column('reset_code', Unicode(255), nullable=True, unique=False),
+    Column('password', Unicode(80), nullable=False),
+    Column('locale', Unicode(7), nullable=True),
+    Column('create_time', DateTime, default=datetime.utcnow),
+    Column('access_time', DateTime, default=datetime.utcnow, onupdate=datetime.utcnow),
+    Column('delete_time', DateTime)
+    )
 
 useremail_table = Table(
-    'useremail', meta.data,
+    'useremail', meta,
     Column('id', Integer, primary_key=True),
     Column('user_id', Integer, ForeignKey('user.id'), nullable=False),
     Column('email', Unicode(255), unique=True),
@@ -14,7 +30,7 @@ useremail_table = Table(
 
 
 def upgrade(migrate_engine):
-    metadata.bind = migrate_engine
+    meta.bind = migrate_engine
     useremail_table.create()
 
 
